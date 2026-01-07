505af27295d6d5cfc323cc243f529dd15fcec530
Aqsagull99 <aqsa.gull.dev.ai99@gmail.com>
2026-01-07 19:51:29 +0500

    Frontend: updated next.config.ts

--- phase-2/frontend/next.config.ts
+++ phase-2/frontend/next.config.ts
@@ -1,7 +1,14 @@
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["better-sqlite3"],
+
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
 };
 
 export default nextConfig;
+
